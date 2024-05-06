9500963688fc57c578016fd94175d8851f45fc92
TravisHFan <travisfanht@gmail.com>
2024-05-06 07:10:26 -0700

    try 10000 port

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+    env: {
+      NEXTAUTH_URL: 'https://localhost:10000'
+    }
+  }
